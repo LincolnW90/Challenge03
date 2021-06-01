6ba0a91b662a03ea3582f15228fcb5f0949f48a8
LincolnW90 <83113735+LincolnW90@users.noreply.github.com>
2021-06-01 18:22:14 -0400

    3.1.5 end

---
+++ assets/game.js
@@ -0,0 +1,14 @@
+// name your robot //
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName)
+
+console.log("this logs a string, good for leaving yourself a message")
+//this will do math and log 20//
+console.log(10+10)
+console.log("Our robot's name is " + playerName)
+// creates start fight warning //
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
