8c5f3aa4c68df233df96a27d3d30954b330b899c
LincolnW90 <83113735+LincolnW90@users.noreply.github.com>
2021-06-13 16:43:58 -0400

    add initial prompts for parameters

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // password length
+  var passwordLength = prompt("How long do you need your password to be? (8-128 characters)");
+  console.log("password length = " + passwordLength);
+  // include lower case?
+  var lowercase = confirm("Include lower-case letters?");
+  console.log("includes lower-case: " + lowercase);
+  // include upper case?
+  var uppercase = confirm("Include upper-case letters?");
+  console.log("includes upper-case: " + uppercase);
+  // include numbers?
+  var numbers = confirm("Include numbers?");
+  console.log("Includes numbers: " + numbers);
+  // include special characters?
+  var special = confirm("Include special characters?");
+  console.log("Includes special chars: " + special);
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
