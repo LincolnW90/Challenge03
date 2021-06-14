03de607ca0d058a76d76e7d2886cabb40fc9eb3d
LincolnW90 <83113735+LincolnW90@users.noreply.github.com>
2021-06-13 21:20:06 -0400

    Got string to print in box, error fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function writePassword() {
       // check password length
       if(passwordLength <8 || passwordLength > 128){
       alert("You must enter an integer between 8-128");
-      writePassword();
+      return;
       } else {
       console.log("password length = " + passwordLength);
       }
@@ -52,26 +52,33 @@ function writePassword() {
         characterpool = characterpool + charsspecial;
       } else {characterpool=characterpool};
 
+      // check that at least 1 option is checked
+      if(lowercase===false && uppercase===false && numbers===false && special===false){
+        alert("You must select at least one option");
+        return;
+      }
+
       console.log("password pool: " + characterpool);
 
-      // define password string
       // begin generating characters
+      var randomstring = "";
       for(i=0; i<passwordLength; i++){
       var randomCharacter = characterpool[Math.floor(Math.random()*characterpool.length)];
+      randomstring = randomstring + randomCharacter;
       console.log(randomCharacter);
       };
 
+      console.log(randomstring);
+      password = randomstring
 
-
-      
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
     };
     // end generatePassword function
 
 
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
